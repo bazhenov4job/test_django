dde860b75bf059a9733db48e54e602d1345a27c3
bazhenov4job <bazhenov4job@gmail.com>
2019-06-09 13:30:00 +0300

    Added links as variables from urls.py

--- test_project/mainapp/views.py
+++ test_project/mainapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/index.html', {'username': 'Vadim', 'age': 30})
+    return render(request, 'mainapp/index.html', {'username': 'Vasya', 'age': 25, 'array': list(range(3))})
 
 def products(request):
     return render(request, 'mainapp/products.html')
