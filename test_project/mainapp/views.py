ab11c9fbdf0510e62edef773e9012a556d4cdc08
bazhenov4job <bazhenov4job@gmail.com>
2019-06-11 13:34:21 +0300

    Added statics url, added vars

--- test_project/mainapp/views.py
+++ test_project/mainapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'username': 'Vadim', 'array': [1, 2, 3]})
 
 def products(request):
     return render(request, 'mainapp/products.html')
