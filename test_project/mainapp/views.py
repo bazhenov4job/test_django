428983572d6ca4758c1bd872c278b7ac438ce265
bazhenov4job <bazhenov4job@gmail.com>
2019-06-13 17:40:53 +0300

    Added menu from menu.json automatic load

--- test_project/mainapp/views.py
+++ test_project/mainapp/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-
+import json, os
 # Create your views here.
 
+JSON_PATH = 'mainapp/json'
+
+def loadMenuFromJSON ():
+    with open(os.path.join(JSON_PATH, 'menu.json'), 'r', encoding="utf-8") as readfile:
+        return json.load(readfile)
+
 def main(request):
-    return render(request, 'mainapp/index.html', {'username': 'Vadim', 'array': [1, 2, 3], 'title': 'главная'})
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu, 'username': 'Vadim', 'array': [1, 2, 3], "title": "главная"}
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu, "title": "продукты"}
+    return render(request, 'mainapp/products.html', context)
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html', {'title': 'контакты'})
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu, "title": "контакты"}
+    return render(request, 'mainapp/contacts.html', context)
