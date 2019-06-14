83353a59940236c9f51597f6c2520d0f221e06ac
bazhenov4job <bazhenov4job@gmail.com>
2019-06-14 10:52:15 +0300

    Filled models.py with DB template

--- test_project/mainapp/models.py
+++ test_project/mainapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.Charfield(verbose_name="имя", max_length=64, unique=True)
+    description = mosels.TextField(verbose_name="описание", blank=True)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeigmKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="имя", max_length=64, unique=True)
+    image = models.ImageField(upload_to="products images", blank=True)
+    short_descr = models.CharField(verbose_name="кратко")
+    description = models.TextField(verbose_name="подробно", blank=True)
+    price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.category.name)
