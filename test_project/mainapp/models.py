d9836849e9107559846759374c6a577336170af2
bazhenov4job <bazhenov4job@gmail.com>
2019-06-14 14:08:37 +0300

    migrations performed

--- test_project/mainapp/models.py
+++ test_project/mainapp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class ProductCategory(models.Model):
-    name = models.Charfield(verbose_name="имя", max_length=64, unique=True)
-    description = mosels.TextField(verbose_name="описание", blank=True)
+    name = models.CharField(verbose_name="имя", max_length=64, unique=True)
+    description = models.TextField(verbose_name="описание", blank=True)
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeigmKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name="имя", max_length=64, unique=True)
     image = models.ImageField(upload_to="products images", blank=True)
-    short_descr = models.CharField(verbose_name="кратко")
+    short_desc = models.CharField(verbose_name="кратко", max_length=64, blank=True)
     description = models.TextField(verbose_name="подробно", blank=True)
     price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
