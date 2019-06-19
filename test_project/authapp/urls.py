a163d72e321faddb06df497bca255825a45246f9
bazhenov4job <bazhenov4job@gmail.com>
2019-06-19 13:27:18 +0300

    performed login action subsequence, modified main

--- test_project/authapp/urls.py
+++ test_project/authapp/urls.py
@@ -7,6 +7,6 @@ app_name = 'authapp'
 urlpatterns = [
     path('login/', authapp.login, name='login'),
     path('logout', authapp.logout, name='logout'),
-    path('register/', authapp.register, name= 'register'),
+    path('register/', authapp.register, name='register'),
     #path('edit/<int:pk>/', authapp.EditView.as_view(), name='edit'),
 ]
--- test_project/authapp/views.py
+++ test_project/authapp/views.py
@@ -8,18 +8,26 @@ from django.urls import reverse
 def login(request):
     title = 'вход'
 
-    login_form = ShopUserLoginForm(data=request.POST)
-    if request.method == "POST" and login_form.is_valid():
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main')) # Выбрасывает нас обратно на глагне
+
+    """login_form = ShopUserLoginForm(data=request.POST)"""
+    """if request.method == "POST" and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
 
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('main'))
+            return HttpResponseRedirect(reverse('main'))"""
 
-    content = {'title': title, 'login_form': login_form}
-    return render(request, 'authapp/login.html', content)
+    """content = {'title': title, 'login_form': login_form}"""
+    return render(request, 'authapp/login.html')
 
 def logout(request):
     auth.logout(request)
