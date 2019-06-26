96a080c56f7ceaa6225c21c2622270fbb9d529d3
bazhenov4job <bazhenov4job@gmail.com>
2019-06-26 16:25:55 +0300

    added new methods to basketapp_basket model, and mainapp.views

--- test_project/basketapp/views.py
+++ test_project/basketapp/views.py
@@ -16,20 +16,33 @@ def basket(request):
 
 
 def basket_add(request, pk):
+    print(pk)
     product = get_object_or_404(Product, pk=pk)
+    old_basket_slot = Basket.objects.filter(user=request.user, product=product).first()
 
-    basket = Basket.objects.filter(user=request.user, product=product).first()
-
-    if not basket:
-        basket = Basket(user=request.user, product=product)
-
-    basket.quantity += 1
-    basket.save()
+    if old_basket_slot:
+        old_basket_slot.quantity += 1
+        old_basket_slot.save()
+    else:
+        new_basket_slot = Basket(user=request.user, product=product)
+        new_basket_slot.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-def basket_remove(request):
-    content = {}
-    return render(request, 'basketapp/basket.html', content)
+def basket_remove(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    print(product.pk)
+    basket_slot = Basket.objects.filter(user=request.user, product=product).first()
+
+    if basket_slot:
+        if basket_slot.quantity == 1:
+            basket_slot.delete()
+        else:
+            basket_slot.quantity -= 1
+            basket_slot.save()
+    else:
+        pass
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
--- test_project/mainapp/views.py
+++ test_project/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 import json, os
 from mainapp.models import Product, ProductCategory
 from basketapp.models import Basket
+import random
 
 # Create your views here.
 
@@ -22,6 +23,7 @@ def main(request):
 def products(request, pk=None):
     basket = []
     if request.user.is_authenticated:
+        # adds to basket var objects inherent to user that is logged and requesting page
         basket = Basket.objects.filter(user=request.user)
 
 
@@ -39,11 +41,16 @@ def products(request, pk=None):
             category = get_object_or_404(ProductCategory, pk=pk)
             products = products.filter(category__pk=pk)
 
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+
     context = {'links_menu': links_menu,
                "title": "продукты",
                "products": products,
                'categories': ProductCategory.objects.all(),
                'basket': basket,
+               'hot_product': hot_product,
+               'same_products': same_products,
                }
     return render(request, 'mainapp/products.html', context)
 
@@ -52,3 +59,20 @@ def contacts(request):
     links_menu = loadMenuFromJSON()
     context = {'links_menu': links_menu, "title": "контакты"}
     return render(request, 'mainapp/contacts.html', context)
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+def get_hot_product():
+    product = Products.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+    return same_products
